dbbdc2c2de27f90011c4fd8539c987f95e33a6b1
Muhammed fayiz | Globalsurf <muhammed.fayiz@globalsurf.ae>
2026-07-10 12:54:57 +0530

    feat: add Lenis smooth scrolling

---
+++ components/provider/ScrollProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
+
+export default function ScrollProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      touchMultiplier: 2,
+      autoRaf: true,
+    });
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
